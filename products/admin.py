88f8825c9d772d2c68a30adbd43010326e5bde04
Soham Joshi <iamsohamj@gmail.com>
2026-09-22 21:41:00 -0400

    Render product catalog and customize admin
    Replaced the placeholder products index response with a database-backed template render using `Product.objects.all()`. Added Bootstrap-based `base.html` and `index.html` templates to display products as cards, and introduced `ModelAdmin` classes for `Product` and `Offer` with useful list columns in Django admin.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 
-admin.site.register(Product)
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','discount') # tuple
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock') # tuple 
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
+
 
--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+
 
 def index(request):
-    return HttpResponse('Hello, world')
+    products = Product.objects.all() # Gets all the products from the database
+    # Product.objects.filter() # Filters the products based on the given parameters
+    # Product.objects.get() # Gets a single product based on the given parameters
+    # Product.objects.save() # Saves the product to the database
+    return render(request, 'index.html', 
+                  {'products': products})
 
 def new(request):
     return HttpResponse('New Products')
